e9311609e5c7261bd995545f0f573b618d645d2c
MIYAKOSHI Kaisei <s1300189@u-aizu.ac.jp>
2023-06-13 11:13:44 +0900

    Dice game: initial revision

---
+++ DiceGame.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+
+public class DiceGame {
+    public static void main(String[] args) {
+	Random rand = new Random();
+	int num, sum = 0;
+	System.out.println("Rolling dice...");
+	for (int i = 1; i <= 2; i++) {
+	    num = rand.nextInt(6) + 1;
+	    sum += num;
+	    System.out.println("Die " + i + ": " + num);
+	}
+	System.out.println("Total value: " + sum);
+    }
+}
