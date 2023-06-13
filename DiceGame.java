01f57d6c2b29d0efaac862a097142d75d2fb550d
MIYAKOSHI Kaisei <s1300189@u-aizu.ac.jp>
2023-06-13 11:36:11 +0900

    Won/lost message modified

--- DiceGame.java
+++ DiceGame.java
@@ -19,9 +19,9 @@ public class DiceGame {
 	}
 	System.out.println("Total value: " + sum);
 	if (sum > 7) {
-		System.out.println("You won!");
+		System.out.println(name + " won!");
 	} else {
-		System.out.println("You lost");
+		System.out.println(name + " lost!");
 	}
     }
 }
